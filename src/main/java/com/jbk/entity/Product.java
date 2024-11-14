8f4f797720ab0734749f9bae2074b3890430d881
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-11-14 23:15:48 +0530

    Update Product.java

--- src/main/java/com/jbk/entity/Product.java
+++ src/main/java/com/jbk/entity/Product.java
@@ -1,115 +1,100 @@
 package com.jbk.entity;
 
-import javax.persistence.*;
-import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
-@Table(name = "Products")
+@Table(name = "product")
 public class Product {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(unique = true, nullable = false)
-    private String productId;
-
-    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int productId;
     private String productName;
-
-    @Column(nullable = false)
-    private int productQuantity;
-
-    @Column(nullable = false)
+    private int productQty;
     private double productPrice;
+    private String mfgString;
+    private String expString;
 
-    @Column(nullable = false)
-    private LocalDate mfgDate;
-
-    @Column(nullable = false)
-    private LocalDate expDate;
-
-    //Default Constructor
-    public Product(){
+    public Product() {
+        // TODO Auto-generated constructor stub
     }
 
-    //Paramterized Constructor
-    public Product (Long id, String productId, String productName, int productQuantity, double productPrice, LocalDate mfgDate, LocalDate expDate) {
-        this.id = id;
+    public Product(String productName, int productQty, double productPrice, String mfgString, String expString) {
+        super();
+        this.productName = productName;
+        this.productQty = productQty;
+        this.productPrice = productPrice;
+        this.mfgString = mfgString;
+        this.expString = expString;
+    }
+
+    public Product(int productId, String productName, int productQty, double productPrice, String mfgString,
+                   String expString) {
+        super();
         this.productId = productId;
         this.productName = productName;
-        this.productQuantity = productQuantity;
+        this.productQty = productQty;
         this.productPrice = productPrice;
-        this.mfgDate = mfgDate;
-        this.expDate = expDate;
+        this.mfgString = mfgString;
+        this.expString = expString;
     }
 
-    // Getters and Setters
-    public Long getId () {
-        return id;
-    }
-
-    public void setId (Long id) {
-        this.id = id;
-    }
-
-    public String getProductId () {
+    public int getProductId() {
         return productId;
     }
 
-    public void setProductId (String productId) {
+    public void setProductId(int productId) {
         this.productId = productId;
     }
 
-    public String getProductName () {
+    public String getProductName() {
         return productName;
     }
 
-    public void setProductName (String productName) {
+    public void setProductName(String productName) {
         this.productName = productName;
     }
 
-    public int getProductQuantity () {
-        return productQuantity;
+    public int getProductQty() {
+        return productQty;
     }
 
-    public void setProductQuantity (int productQuantity) {
-        this.productQuantity = productQuantity;
+    public void setProductQty(int productQty) {
+        this.productQty = productQty;
     }
 
-    public double getProductPrice () {
+    public double getProductPrice() {
         return productPrice;
     }
 
-    public void setProductPrice (double productPrice) {
+    public void setProductPrice(double productPrice) {
         this.productPrice = productPrice;
     }
 
-    public LocalDate getMfgDate () {
-        return mfgDate;
+    public String getMfgString() {
+        return mfgString;
     }
 
-    public void setMfgDate (LocalDate mfgDate) {
-        this.mfgDate = mfgDate;
+    public void setMfgString(String mfgString) {
+        this.mfgString = mfgString;
     }
 
-    public LocalDate getExpDate () {
-        return expDate;
+    public String getExpString() {
+        return expString;
     }
 
-    public void setExpDate (LocalDate expDate) {
-        this.expDate = expDate;
+    public void setExpString(String expString) {
+        this.expString = expString;
     }
 
     @Override
-    public String toString () {
-        return "Product{" +
-                "id=" + id +
-                ", productId='" + productId + '\'' +
-                ", productName='" + productName + '\'' +
-                ", productQuantity=" + productQuantity +
-                ", productPrice=" + productPrice +
-                ", mfgDate=" + mfgDate +
-                ", expDate=" + expDate +
-                '}';
+    public String toString() {
+        return "Product [productId=" + productId + ", productName=" + productName + ", productQty=" + productQty
+                + ", productPrice=" + productPrice + ", mfgString=" + mfgString + ", expString=" + expString + "]";
     }
+
 }
